c11d221bff40dffae1598ae252ffd98501dc4598
Tiago Martinho <shark11833@sharkcoders.pt>
2026-05-29 19:31:54 +0100

    versao 01 super tiago

---
+++ super_tiago.js
@@ -0,0 +1,30 @@
+
+const mycanvas = document.getElementById("mycanvas")
+const ctx = mycanvas.getContext("2d")
+const cenarios_fundos = [
+    "imagens/super_tiago/cenario01_fundo.png",
+    "imagens/super_tiago/cenario02_fundo.png",
+    "imagens/super_tiago/cenario03_fundo.png"
+]
+
+function loadScene(id) {
+    const cenario01_fundo = new Image()
+    cenario01_fundo.src = cenarios_fundos[id];
+    cenario01_fundo.onload = () => {
+        ctx.drawImage(cenario01_fundo, 0, 0)
+    }
+}
+
+let sceneId = 0
+loadScene(sceneId)
+
+addEventListener("keyup", (ev) => {
+    if (ev.code === "ArrowRight") {
+        if (sceneId < 2) sceneId = sceneId + 1
+        loadScene(sceneId)
+    }else if (ev.code === "ArrowLeft") {
+        if (sceneId > 0) sceneId = sceneId - 1
+        loadScene(sceneId)
+    } 
+})
+
